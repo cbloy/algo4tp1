805fdd49d27f93120217f8c993b7fa3deec92a11
djbrait@gmail.com <djbrait@gmail.com@890c8118-57cf-8153-a632-4b525b321635>
2011-05-07 01:24:39 +0000

    DEFINICION DE TABLAS

--- tp/TP.cbl
+++ tp/TP.cbl
@@ -50,5 +50,24 @@
 		   03 FILLER					PIC X(5) 
 										VALUE 'Hoja '.
 		   03 ENC-N-HOJA				PIC 99.		   
-		
+
+      ************		   
+      *  TABLAS  *
+      ************
+	   01  TABLA-AUTOS.
+	       05  TABLA-AUT                OCCURS 300 TIMES.
+		       09  TABLA-AUTOS-REG.
+			       11  AUTO-PATENTE     PIC X(6).
+				   11  AUTO-DESC        PIC X(30).
+				   11  AUTO-MARCA       PIC X(20).
+				   11  AUTO-COLOR       PIC X(10).
+				   11  AUTO-TAMANIO     PIC X.
+				   11  AUTO-IMPORTE     PIC 9(4)V99.
+				   
+       01  TABLA-ESTAD.
+	       05  ESTAD-MARCAS             OCCURS 100 TIMES.
+		       09  ESTAD-MARCA          PIC X(10).
+			   09  ESTAD-MESES          OCCURS 12 TIMES.
+			       11  ESTAD-MES        PIC 9(3).
+				   
        PROCEDURE DIVISION.
