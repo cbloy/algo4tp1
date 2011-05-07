d03ceadab65260f7d4cb1b4f60f2def7084ce803
djbrait@gmail.com <djbrait@gmail.com@890c8118-57cf-8153-a632-4b525b321635>
2011-05-07 02:28:05 +0000

    Se abre el archivo autos.. pero no se lee bien.

--- tp/TP.cbl
+++ tp/TP.cbl
@@ -8,23 +8,52 @@
        SPECIAL-NAMES.
                  DECIMAL-POINT IS COMMA.
       *      	  
-      * INPUT-OUTPUT SECTION.
-      * FILE-CONTROL.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT AUTOS
+               ASSIGN TO "..\AUTOS.TXT" 
+		       ORGANIZATION IS LINE SEQUENTIAL
+			   ACCESS MODE IS SEQUENTIAL
+		       FILE STATUS IS FS-AUTOS.
+		   
 	   
 	   DATA DIVISION.
-      * FILE SECTION.
+	   
+       FILE SECTION.
+       FD AUTOS.
+	   01  AUT-REG.
+	       05  AUT-PATENTE        PIC X(6).
+		   05  AUT-DESC           PIC X(30).
+		   05  AUT-MARCA          PIC X(20).
+		   05  AUT-COLOR          PIC X(10).
+		   05  AUT-TAMANIO        PIC X.
+		   05  AUT-IMPORTE        PIC 9(4)V99.
 	   
 	   WORKING-STORAGE SECTION.
+	   
+      *****************
+      *  FILE STATUS  *	  
+      *****************
+	   01  FS-AUTOS                     PIC X(02).
+	       88  FS-AUTOS-OK              VALUE '00'.
+           88  FS-AUTOS-FIN             VALUE '10'.
+ 
+      * PARA CHEQUEO DE FILE STATUS
+       01  FILE-STATUS.
+          05  FS                       PIC X(02).
+          05  FS-NOMBRE                PIC X(08).
+          05  FS-FUNCION               PIC X(05).	  
+		   
       ************		   
       *  CLAVES  *
       ************
 	   01  CLAVE-ANTERIOR.
-	       05  PATENTE            PIC X(6).
-		   05  FECHA              PIC 9(8).
+	       05  PATENTE                  PIC X(6).
+		   05  FECHA                    PIC 9(8).
 	   
        01  CLAVE-ACTUAL.
-	       05  PATENTE            PIC X(6).
-		   05  FECHA              PIC 9(8).	   
+	       05  PATENTE                  PIC X(6).
+		   05  FECHA                    PIC 9(8).	   
 		   
       **************		   
       *  LISTADOS  *
@@ -71,3 +100,44 @@
 			       11  ESTAD-MES        PIC 9(3).
 				   
        PROCEDURE DIVISION.
+	   PGM.		
+           DISPLAY "INICIA EL PROGRAMA".
+           PERFORM 1000-INICIO.
+		   PERFORM 8400-LEER-AUTOS.
+		   DISPLAY AUT-REG
+           DISPLAY "FINALIZA EL PROGRAMA". 
+		   STOP RUN.
+		   
+       1000-INICIO.
+           PERFORM 1100-ABRIR-ARCHIVOS.
+	  
+       1100-ABRIR-ARCHIVOS.
+		   OPEN INPUT  AUTOS.
+           MOVE FS-AUTOS      TO FS.
+           MOVE "AUTOS   "    TO FS-NOMBRE.
+           MOVE "ABRIR"       TO FS-FUNCION.
+           PERFORM 8900-CHECK-FILE-STATUS.
+       
+	   8400-LEER-AUTOS.
+           READ AUTOS AT END SET FS-AUTOS-FIN TO TRUE.
+		   
+           IF NOT FS-AUTOS-OK AND NOT FS-AUTOS-FIN
+               DISPLAY 'ERROR AL INTENTAR LEER AUTOS'
+               PERFORM 9999-CANCELAR-PROGRAMA
+           END-IF.
+       8900-CHECK-FILE-STATUS.
+           IF FS NOT EQUAL "00"
+              DISPLAY "CANCELACION POR ERROR"
+              DISPLAY "EN ARCHIVO: " FS-NOMBRE
+              DISPLAY "FILESTATUS: " FS
+              DISPLAY "AL INTENTAR: " FS-FUNCION
+              PERFORM 9999-CANCELAR-PROGRAMA
+           END-IF.	
+		   
+       9000-FINAL.
+           CLOSE AUTOS.
+           
+       9999-CANCELAR-PROGRAMA.
+           PERFORM 9000-FINAL.
+           DISPLAY "SALIDA POR CANCELACION DE PROGRAMA".
+		   STOP RUN.		   
